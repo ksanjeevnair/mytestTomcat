ec5bfb76b98341b44eb64dfeb0a06ce4f4340fb1
fedora <fedora@localhost.localdomain>
2014-05-01 18:34:14 +0200

    adding test cases

--- ironhide/http_test.pl
+++ ironhide/http_test.pl
@@ -1,24 +1,13 @@
-require LWP::UserAgent;
-my $URL=$ARGV[0];
-my $retry_count=20;
- 
-my $ua = LWP::UserAgent->new;
-$ua->timeout(50);
-$ua->ssl_opts( verify_hostname => 0 );
-do { 
-	my $response = $ua->get("$URL");
- 
-	if ($response->is_success) {
-    		print $response->content ;  
-		print "\n";
-    		print " The server looks fine\n";  
-		exit;
-	}
-	else {
-		print "Try number $retry_count failed \n";
-		sleep 10;
-	}
-	$retry_count--;
-   }until ($retry_count == 0 ); 
-    	print " Its been 2 minutes and looks like the server is not up, please login to server and check server logs \n";
+#!/usr/bin/perl -w
+use strict;
+
+use Test::More qw( no_plan );
+
+sub main{
+
+diag "Start URL check test";
+system "sh http_test.sh";
+ok ( "$?" == 0 , "URL fetch Test");
+}
+main;
 
